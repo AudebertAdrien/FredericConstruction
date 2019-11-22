4d25ff6dc651ab18bf7380baa65d8b18b326b1f3
AudebertAdrien <audebertadrien.pro@gmail.com>
2019-11-23 00:06:26 +0100

    remove all transition navbar-contact

--- app/assets/javascripts/homes/index.js
+++ app/assets/javascripts/homes/index.js
@@ -1,5 +1,7 @@
 Paloma.controller("Homes", {
   index: function() {
+
+    //Navbar transparent to color on Home page
     navbar = document.querySelector(".navbar");
     heightBgHome = document
       .querySelector("#bg-home-top")
@@ -16,25 +18,29 @@ Paloma.controller("Homes", {
       }
     };
     window.addEventListener("scroll", onScroll);
+    ////////////////////////////////
 
+/*
     navbarToggler = document.querySelector(".navbar-toggler");
     navbarCollapse = document.querySelector("#navbarNav")
-
     navbarContact = document.querySelector(".navbar-contact");
 
     navAppendChild = document.querySelector(".nav-append-child");
+    col2 = navAppendChild.querySelector(".col:nth-child(2)");
 
     isToggler = function() {
       if (!navbarCollapse.classList.contains("show")) {
         navbarContact.parentElement.removeChild(navbarContact);
-        navAppendChild.appendChild(navbarContact);
       } else {
-        navAppendChild.removeChild(navbarContact);
-        col2 = navAppendChild.querySelector(".col:nth-child(2)");
+        navbarContact.style.display = "none";
+        col2.classList.remove("justify-content-between");
+        col2.classList.add("justify-content-end");
         col2.insertAdjacentElement("afterbegin", navbarContact);
+        navbarContact.classList.add("contact-appear")
       }
     };
 
     navbarToggler.addEventListener("click", isToggler);
+*/
   }
 });
