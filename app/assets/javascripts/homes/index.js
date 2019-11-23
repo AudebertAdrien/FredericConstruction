a68ae9d6867084da3592c0dc901484731af36fe3
AudebertAdrien <audebertadrien.pro@gmail.com>
2019-11-23 12:11:40 +0100

    it starts to become clean

--- app/assets/javascripts/homes/index.js
+++ app/assets/javascripts/homes/index.js
@@ -19,28 +19,5 @@ Paloma.controller("Homes", {
     };
     window.addEventListener("scroll", onScroll);
     ////////////////////////////////
-
-/*
-    navbarToggler = document.querySelector(".navbar-toggler");
-    navbarCollapse = document.querySelector("#navbarNav")
-    navbarContact = document.querySelector(".navbar-contact");
-
-    navAppendChild = document.querySelector(".nav-append-child");
-    col2 = navAppendChild.querySelector(".col:nth-child(2)");
-
-    isToggler = function() {
-      if (!navbarCollapse.classList.contains("show")) {
-        navbarContact.parentElement.removeChild(navbarContact);
-      } else {
-        navbarContact.style.display = "none";
-        col2.classList.remove("justify-content-between");
-        col2.classList.add("justify-content-end");
-        col2.insertAdjacentElement("afterbegin", navbarContact);
-        navbarContact.classList.add("contact-appear")
-      }
-    };
-
-    navbarToggler.addEventListener("click", isToggler);
-*/
   }
 });
