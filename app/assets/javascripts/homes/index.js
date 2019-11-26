dd07b97be5539af8c830cf3599f1c6d57ff9e250
AudebertAdrien <audebertadrien.pro@gmail.com>
2019-11-26 17:21:12 +0100

    lessthan md navbar toggler -> container fluid

--- app/assets/javascripts/homes/index.js
+++ app/assets/javascripts/homes/index.js
@@ -1,5 +1,19 @@
 Paloma.controller("Homes", {
   index: function() {
+    navLink = document.querySelector(".nav-link");
 
+    mediumDevise = window.matchMedia("(max-width: 768px)")
+
+    function lessThanMd(e) {
+      if (e.matches) {
+        navLink.classList.remove("container")
+        navLink.classList.add("container-fluid")
+      } else {
+        navLink.classList.add("container")
+        navLink.classList.remove("container-fluid")
+      }  
+    }
+
+    mediumDevise.addListener(lessThanMd)
   }
 });
