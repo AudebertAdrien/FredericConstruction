b46b5427cd8cf89d7185d2f28ef49e416cdbe50f
AudebertAdrien <audebertadrien.pro@gmail.com>
2019-11-26 17:02:02 +0100

    general ajustment

--- app/assets/javascripts/homes/index.js
+++ app/assets/javascripts/homes/index.js
@@ -1,23 +1,5 @@
 Paloma.controller("Homes", {
   index: function() {
-/*
-    //Navbar transparancy to color
-    navbar = document.querySelector(".navbar");
-    heightBgHome = document.querySelector("#bg-home-top").getBoundingClientRect().height;
-    hasScrollclass = navbar.classList.contains("navbar-color-js");
 
-    onScroll = function() {
-      if (window.pageYOffset > heightBgHome - navbar.offsetHeight && !hasScrollclass) {
-        navbar.classList.add("navbar-color-js");
-        hasScrollclass = true;
-      } else if (window.pageYOffset < heightBgHome - navbar.offsetHeight  && hasScrollclass) {
-        navbar.classList.remove("navbar-color-js");
-        hasScrollclass = false;
-      }
-    };
-    window.addEventListener("scroll", onScroll);
-
-    //////////////
-*/
   }
 });
