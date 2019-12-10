26f9e8ca25bcd24e10af86223ceb2d418a4f2055
AudebertAdrien <audebertadrien.pro@gmail.com>
2019-12-10 14:33:59 +0100

    jumbotron bg-image resize

--- app/assets/javascripts/homes/index.js
+++ app/assets/javascripts/homes/index.js
@@ -4,7 +4,7 @@ Paloma.controller("Homes", {
     navLink = document.querySelector(".line-link");
     blockLogoContact = document.querySelector(".blockLogoContact");
     navbarToggler = document.querySelector(".navbar-toggler")
-    mediumDevise = window.matchMedia("(max-width: 768px)");
+    mediumDevise = window.matchMedia("(max-width: 767px)");
 
 
     // Navbar fluid for resize
