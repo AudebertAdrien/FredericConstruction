11407452c659da6e107184ad2003b65a4082cd07
AudebertAdrien <audebertadrien.pro@gmail.com>
2019-12-23 18:29:15 +0100

    new show create

--- app/controllers/realisations_controller.rb
+++ app/controllers/realisations_controller.rb
@@ -1,2 +1,18 @@
 class RealisationsController < ApplicationController
+    def index
+        @realisations = Realisation.all
+    end
+
+    def new
+    end
+
+    def show
+        @realisation = Realisation.find(params[:id])
+    end
+
+    def create
+        puts "#" * 100
+        puts params
+        puts "#" * 100
+    end
 end
