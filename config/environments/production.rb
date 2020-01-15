eb896d51cf49d2596c939c8cfa827a2f1b85b320
AudebertAdrien <audebertadrien.pro@gmail.com>
2020-01-15 16:48:12 +0100

    desperate new test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,11 @@ Rails.application.configure do
   #/////////
 
   #ActionMailer amazon SES sandbox : 200 mails per 24h and 1 mail per second
-  client = Aws::SES::Client.new(region: 'AMAZON_SERVER_NAME')
+  client = Aws::SES::Client.new(
+    :user_name => ENV['AMAZON_USER_NAME']
+    :smtp_username => ENV['AMAZON_SMTP_USERNAME']
+    :smtp_password => ENV['AMAZON_SMTP_PASSWORD']
+  )
 
   puts "#" * 100
   puts client
