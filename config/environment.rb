4ee02b7f8e553206aec10019489aa85fe46d436f
AudebertAdrien <audebertadrien.pro@gmail.com>
2020-01-16 12:11:16 +0100

    change code stmp file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# SMTP AWS SES
+ActionMailer::Base.smtp_settings = {
+    address: 'email-smtp.eu-west-1.amazonaws.com',
+    port: 587,
+    user_name: ENV['SES_SMTP_USERNAME'], # Your SMTP user
+    password: ENV['SES_SMTP_PASSWORD'], # Your SMTP password
+    authentication: :login,
+    enable_starttls_auto: true
+}
